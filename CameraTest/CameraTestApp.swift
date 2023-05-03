8c1bcc92fbd7910d91190ffe0fac3986dd535ef4
Alex Luna <contato@tarrask.com>
2023-05-03 12:12:49 -0300

    Initial Commit

---
+++ CameraTest/CameraTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraTestApp.swift
+//  CameraTest
+//
+//  Created by tarrask on 03/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CameraTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
